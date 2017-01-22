018db5c54349c96e61c2e3c1f996d3119ce605c7
Derrick Thai <derrickthai@Derricks-MacBook-Pro.local>
2017-01-22 13:58:52 -0500

    Initial Commit

---
+++ Project Euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project Euler
+//
+//  Created by Derrick Thai on 2017-01-22.
+//  Copyright © 2017 Derrick Thai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
